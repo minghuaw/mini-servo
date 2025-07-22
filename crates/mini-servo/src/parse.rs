d8983474361a673629ded6067e558b63558cc485
minghuaw <michael.wu1107@gmail.com>
2025-07-22 10:08:46 +0800

    parse html with blitz-dom
    Signed-off-by: minghuaw <michael.wu1107@gmail.com>

--- crates/mini-servo/src/parse.rs
+++ crates/mini-servo/src/parse.rs
@@ -1,21 +1,11 @@
 use std::io::Cursor;
 
+use blitz_dom::{BaseDocument, DocumentConfig};
+use blitz_html::DocumentHtmlParser;
 use html5ever::{interface::TreeSink, tendril::TendrilSink, ParseOpts};
 use rcdom::RcDom;
 
 use crate::core::Dom;
-
-// pub fn parse_html<D, S>(html: &str, sink: S, opts: ParseOpts) -> std::io::Result<D> 
-// where 
-//     D: Dom,
-//     S: TreeSink,
-// {
-//     html5ever::parse_document(sink, opts)
-//         .from_utf8()
-//         .read_from(&mut html.as_bytes())?;
-
-//     todo!()
-// }
 
 pub trait ParseHtml: Sized {
     type Error;
@@ -34,15 +24,40 @@ impl ParseHtml for RcDom {
     }
 }
 
+impl ParseHtml for BaseDocument {
+    type Error = std::io::Error;
+
+    fn parse_html(html: &str, opts: ParseOpts) -> Result<Self, Self::Error> {
+        let mut reader = Cursor::new(html);
+
+        let config = DocumentConfig::default();
+        let mut doc = BaseDocument::new(config);
+        let sink = DocumentHtmlParser::new(&mut doc);
+
+        html5ever::parse_document(sink, opts)
+            .from_utf8()
+            .read_from(&mut reader)?;
+
+        Ok(doc)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use blitz_dom::BaseDocument;
     use rcdom::RcDom;
 
     use crate::parse::ParseHtml;
 
+    const SIMPLE_TEST_HTML: &str = "<p>Hello, world!</p>";
+
     #[test]
     fn test_rcdom_parse_html() {
-        let test_html = "<p>Hello, world!</p>";
-        let _parsed = RcDom::parse_html(test_html, Default::default()).unwrap();
+        let _parsed = RcDom::parse_html(SIMPLE_TEST_HTML, Default::default()).unwrap();
+    }
+
+    #[test]
+    fn test_blitz_dom_parse_html() {
+        let _parsed = BaseDocument::parse_html(SIMPLE_TEST_HTML, Default::default()).unwrap();
     }
 }
